7b765b119a626eca5d7e94100cc506018024f67e
dmitryosipchuk-switch <dmitry.osipchuk@weareswitch.com>
2021-05-06 11:54:55 +0100

    Setting up project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { MyContext } from './context';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	static contextType = MyContext;
+	render() {
+		return (
+			<div className="wrapper">
+				<div className="center-wrapper">
+					<h1>Who pays the bill ?</h1>
+					{console.log(this.context.state)}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/context/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+const MyContext = React.createContext();
+
+class MyProvider extends Component {
+	state = {
+		stage: 1,
+		players: [],
+		result: '',
+	};
+
+	render() {
+		return (
+			<>
+				<MyContext.Provider
+					value={{
+						state: this.state,
+						addPlayer: this.addPlayerHandler,
+						removePlayer: this.removePlayerHandler,
+						next: this.nextHandler,
+						getNewLooser: this.generateLooser,
+						reset: this.resetGame,
+					}}
+				>
+					{this.props.children}
+				</MyContext.Provider>
+			</>
+		);
+	}
+}
+
+export { MyContext, MyProvider };
