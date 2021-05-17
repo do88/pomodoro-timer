bc15c2ead0ce0f4ef3207dcc30c57eb2f1d6cbeb
do88 <dmitry@dmitry-o.co.uk>
2021-05-17 06:50:19 +0100

    Footer development

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { MyContext } from './context';
 
 // Project components
 import Header from './components/header';
+import Footer from './components/footer';
 
 // App styling
 import './index.css';
@@ -16,7 +17,7 @@ class App extends React.Component {
 			<div className="wrapper">
 				<Header />
 				<main className="timer">Timer</main>
-				<footer className="footer">Footer</footer>
+				<Footer />
 			</div>
 		);
 	}
---
+++ src/components/footer.js
@@ -0,0 +1,18 @@
+import '../styles/Footer.css';
+
+const Header = () => {
+	return (
+		<footer className="footer">
+			© 2021 Dmitry Osipchuk - Find me on{' '}
+			<a href="https://github.com/do88" target="_blank">
+				GitHub
+			</a>{' '}
+			or{' '}
+			<a href="https://www.linkedin.com/in/osipchuk/" target="_blank">
+				LinkedIn
+			</a>
+		</footer>
+	);
+};
+
+export default Header;
